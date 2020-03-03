e41fcb78aa3ad7520d1b288b5435dfd52a5f8052
virtualizacion08 <gabyjaque@hotmail.com>
2020-03-03 17:25:58 -0600

    Initial base to create graphs

---
+++ E4/node.c
@@ -0,0 +1,41 @@
+// See node.h for more info
+// Author: 
+// File: node.c
+// Date: 17/02/20
+
+// ------------------------------------------
+// System and aplication specific headers
+// ------------------------------------------
+#include "node.h"
+
+
+// -----------------------------
+// Private elements
+// -----------------------------
+
+/* Private macros and constants */
+
+/* Private types */
+
+/* Private global variables */
+
+/* Private functions */
+
+
+// -----------------------------
+// Public elements
+// -----------------------------
+
+/* Implementation of the public functions */
+
+Node_t *newNode( int id, float cost, Node_t *nextN[], int8_t c_state ) {
+
+}
+
+bool newConnection( Node_t *nodes[], Node_t **base ) {
+
+}
+
+Node_t **findNode( int id[] ) {
+    
+}
---
+++ E4/node.h
@@ -0,0 +1,59 @@
+// 
+// Author: 
+// File: node.h
+// Date: 03/03/20
+
+#ifndef NODE_H
+#define NODE_H
+
+// ------------------------------------------
+// System specific headers
+// ------------------------------------------
+#include <stdbool.h>
+#include <inttypes.h>
+
+
+// -----------------------------
+// Public interface
+// -----------------------------
+
+/* Types declarations */
+
+typedef struct Node {
+    int id;
+    float cost;
+    struct Node *nextN[];
+    int8_t c_state;
+} Node_t;
+
+/* Function prototypes */
+
+/**
+ * Creates a new node.
+ * 
+ * @param id Node's id.
+ * @param cost Arrival cost time.
+ * @param nextN Nodes to connect.
+ * @param c_state Connections' state.
+ * @return Pointer to the new node.
+ */
+Node_t *newNode( int id, float cost, Node_t *nextN[], int8_t c_state );
+
+/**
+ * Assigns nodes to a root node.
+ * 
+ * @param nodes Nodes that will be assign to the base node.
+ * @param base Root node.
+ * @return True if all assignments were correctly created; otherwise, false.
+ */
+bool newConnection( Node_t *nodes[], Node_t **base );
+
+/**
+ * Find a set of nodes using the given ids. 
+ * 
+ * @param id Ids to search.
+ * @return Found nodes.
+ */
+Node_t **findNode( int id[] );
+
+#endif
