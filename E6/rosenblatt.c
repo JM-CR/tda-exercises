861d9e1b08f90f81ff50da8ad97e92bf30097ff4
Josue Mosh <msi_98_98@yahoo.com.mx>
2020-04-11 11:27:46 -0500

    Initial set up for implement the Hebb's algorithm

---
+++ E6/rosenblatt.c
@@ -0,0 +1,29 @@
+// See rosenblatt.h for more info
+// Author: Josue Mosiah Contreras Rocha
+// File: rosenblatt.c
+// Date: 11/04/20
+
+// ------------------------------------------
+// System and aplication specific headers
+// ------------------------------------------
+#include "rosenblatt.h"
+
+
+// -----------------------------
+// Private elements
+// -----------------------------
+
+/* Private macros and constants */
+
+/* Private types */
+
+/* Private global variables */
+
+/* Private functions */
+
+
+// -----------------------------
+// Public elements
+// -----------------------------
+
+/* Implementation of the public functions */
---
+++ E6/rosenblatt.h
@@ -0,0 +1,31 @@
+// This library implements the Hebb's algorithm for neuronal networks.
+// Author: Josue Mosiah Contreras Rocha
+// File: rosenblatt.h
+// Date: 11/04/20
+
+#ifndef ROSENBLATT_H
+#define ROSENBLATT_H
+
+// -----------------------------
+// System headers
+// -----------------------------
+
+
+// -----------------------------
+// Public interface
+// -----------------------------
+
+/* Constants */
+
+/* Types declarations */
+
+/* Global variables */
+
+/* Function prototypes */
+
+/**
+ * Function's documentation.
+ */
+
+
+#endif
